fe3fda4175f5ea2dcba52a3721b35c4f047e3833
Triathlon-staf <serverbersama94@gmail.com>
2026-02-22 18:31:08 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // ================= SUPABASE INIT =================
-const SUPABASE_URL = "ISI_PROJECT_URL";
-const SUPABASE_KEY = "ISI_ANON_KEY";
+const SUPABASE_URL = "https://fymofqgapfcpbhexhgvy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_DdAzmn3X3NlhJNpEmul0ig_c57EUCxh";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
